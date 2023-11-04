f15eb49856e08f26f4c39ab520ff3af846537e39
saku-bruh <notwolfyy@gmail.com>
2023-11-04 19:15:56 +0200

    extra-sagami: audio: Guard and add libs for DIRAC
    * Blobs imported from:
    https://github.com/lolipuru/vendor_xiaomi_perseus/blob/main/sdm845-common/proprietary/vendor/
    https://dumps.tadiphone.dev/dumps/redmi/alioth/-/tree/missi_phoneext4_global-user-13-TKQ1.220829.002-V14.0.7.0.TKHMIXM-release-keys/vendor/
    
    * Dolby is still disabled, but we added a flag to guard it (this flag is also used on hw_sony) so it will work hand-in-hand with it
    
    * Also added DIRAC libs under the same flag
    
    Change-Id: Ib01da466c475cdd5a4723b3fa31cd886ea925ead
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- extra.mk
+++ extra.mk
@@ -24,8 +24,14 @@ endif
 PRODUCT_SOONG_NAMESPACES += \
     $(EXTRA_PATH)
 
-# Dolby Sound
-    $(call inherit-product, $(EXTRA_PATH)/extra/dolby/dolby.mk)
+# Flags
+TARGET_SUPPORTS_SOUND_ENHANCEMENT ?= true
+
+# Sound Enhancements
+ifeq ($(TARGET_SUPPORTS_SOUND_ENHANCEMENT),true)
+    $(call inherit-product, $(EXTRA_PATH)/extra/audio/dolby/dolby.mk)
+    $(call inherit-product, $(EXTRA_PATH)/extra/audio/dirac/dirac.mk)
+endif
 
 # Sony Framework
 PRODUCT_COPY_FILES += \
