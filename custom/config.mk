d51c1cd9ec4db8b3639d2710988f28acff5e8421
someone5678 <someone5678@users.noreply.github.com>
2024-04-19 23:58:32 +0900

    extra: custom: Guard it
    Change-Id: I27dc30e621ac513e3d6f28840d18eb20f81da9c1

--- custom/config.mk
+++ custom/config.mk
@@ -1,3 +1,5 @@
+TARGET_SHIPS_CUSTOM_DOLBY ?= true
+ifeq ($(TARGET_SHIPS_CUSTOM_DOLBY),true)
 # Modified daxService
 PRODUCT_PACKAGES += \
     daxServiceCustom
@@ -5,3 +7,4 @@ PRODUCT_PACKAGES += \
 # Modified DolbySound
 PRODUCT_PACKAGES += \
     DolbySoundCustom
+endif
